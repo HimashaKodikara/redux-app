37d22d4f72d2832e4504a9f6d063d64ccf8f6ecb
19840769 <kodikarah.21@uom.lk>
2026-03-31 22:56:59 +0530

    (Change)

---
+++ src/components/cakeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function cakeContainer() {
+  return (
+    <div>
+        <h2>Number of cakes</h2>
+        <button>Buy Cake</button>
+    </div>
+
+  )
+}
+
+export default cakeContainer
