ab8d77e28629309b82b6d0da4b00e0d438a7459e
19840769 <kodikarah.21@uom.lk>
2026-04-01 23:47:44 +0530

    feat: add HooksCakeContainer component and integrate it into the main App component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./App.css";
 import CakeContainer from "./components/cakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer></HooksCakeContainer>
         <CakeContainer />
       </div>
     </Provider>
