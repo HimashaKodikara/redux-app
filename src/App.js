7f1d688d1809d5c7ad5b2ae0358a1abaee866631
19840769 <kodikarah.21@uom.lk>
2026-04-02 22:53:11 +0530

    feat: add dynamic cake purchasing and conditional item container components to Redux store

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import "./App.css";
 import CakeContainer from "./components/cakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IcecreamContainer from "./components/IcecreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/itemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer></HooksCakeContainer>
         <CakeContainer />
         <IcecreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
